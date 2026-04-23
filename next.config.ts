22a0e668980bc88d9513c64be19d9ea70c92aaaa
surapas <surapas@MacBook-Pro-khxng-surapas.local>
2026-04-23 08:49:16 +0700

    config redirect workshop path

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/claude-101-workshop",
+        destination:
+          "https://www.notion.so/langchain-introduction/Workshop-Claude-101-34bcb6948c72803da552de66a780a156?source=copy_link",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
